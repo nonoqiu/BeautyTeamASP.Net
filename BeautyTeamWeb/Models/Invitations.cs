e0da6da1a39fc3b5ffbc4980a7816e2734d7490d
mma <mma@DESKTOP-E0L8DM0>
2016-06-04 13:55:40 +0800

    nothing

--- BeautyTeamWeb/Models/Invitations.cs
+++ BeautyTeamWeb/Models/Invitations.cs
@@ -6,14 +6,26 @@ using System.Web;
 
 namespace BeautyTeamWeb.Models
 {
-    public class Invitation
+    public abstract class Invitation
     {
         public virtual int InvitationId { get; set; }
 
-        public virtual int GroupId { get; set; }
-        public virtual Group Group { get; set; }
-
+        //Target
         public virtual string ObisoftUserId { get; set; }
         public virtual ObisoftUser ObisoftUser { get; set; }
+    }
+    //Person to person
+    public class P2PInvitation : Invitation
+    {
+        //From
+        public virtual int FriendsPartId { get; set; }
+        public virtual FriendsPart FriendsPart { get; set; }
+    }
+    //Invite to Group
+    public class G2PInvitation : Invitation
+    {
+        //From
+        public virtual int GroupId { get; set; }
+        public virtual Group Group { get; set; }
     }
 }
